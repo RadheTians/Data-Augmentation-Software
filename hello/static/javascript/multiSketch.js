4bd874b401792a8b7e0bc1136de43197a9fc20f3
RadheTians <radheramantiwari87@gmail.com>
2019-12-20 18:28:31 +0530

    new version

--- hello/static/javascript/multiSketch.js
+++ hello/static/javascript/multiSketch.js
@@ -3,7 +3,8 @@ let xmin=0,ymin=0,xmax=0,ymax=0;
 let xbox,ybox;
 let flag=0;
 let reader = [];
-let x=0;
+let abc;
+let a=0;
 function setup() {
     var canvas = createCanvas(1000, 1000);
     canvas.doubleClicked(setXY);
@@ -14,17 +15,10 @@ function setup() {
 }
 
 function draw() {
-
+    
     if(reader.length!=0){
-        print(reader);
-        reader[x].onload = function (e) {
-            createImg(e.target.result,'');
-            $('#img').attr('src', e.target.result); 
-        };
-        image(img, 0, 0);
-        
+        image(reader[a], 0,0);
     }
-   
     if(xmin!=0 && ymin!=0 && xmax!=0 && ymax!=0){
         document.getElementById("xmax").value = xmax;
         document.getElementById("ymax").value = ymax;
@@ -37,18 +31,55 @@ function draw() {
 
 function readImageURL(input) {
 
-  	if (input.files) {
+    if (input.files) {
         for(var i=0;i<input.files.length;i++){
-            reader[i] = new FileReader();
-            reader[i].readAsDataURL(input.files[i]);
+            reader[i] = loadImage(URL.createObjectURL(input.files[i]));
+            
         }
-        
     }
+   
+  	// if (input.files) {
+    //     for(var i=0;i<input.files.length;i++){
+    //         reader[i] = new FileReader();
+    //         reader[i].onload = function (e) {
+    //             $('#img').attr('src', e.target.result);
+    //         };
+    //         reader[i].readAsDataURL(input.files[i]);
+    //         print(input.files[i]);
+    //         var k = loadImage(input.files[i].src);
+    //     }
+    //     image(k, 0,0);
+        
+    // }
+//   var files   = document.querySelector('#image').files;
+//   print(files[8]);
+//   function readAndPreview(file) {
+
+//     // Make sure `file.name` matches our extensions criteria
+//     if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+//       var reader = new FileReader();
+
+//       reader.addEventListener("load", function () {
+//             abc =loadImage(this.result);
+            
+//       }, false);
+//       input[i]=abc;
+//       print(i);
+//       i++;
+//       reader.readAsDataURL(file);
+//     }
+
+//   }
+
+//   if (files) {
+//     [].forEach.call(files, readAndPreview);
+//   }
 }
 
 function doubleClicked() {
-    x++;
+
     if(flag==1){
+        a++;
         xmax = mouseX;
         ymax = mouseY;
         xbox = mouseX-xmin;
